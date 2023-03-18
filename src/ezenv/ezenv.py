08713382f450e53fb6b5928458512b4364ba3c57
EnkhAmar <enkhamar1220@gmail.com>
2023-03-19 02:30:11 +0800

    v.0.0.6

--- src/ezenv/ezenv.py
+++ src/ezenv/ezenv.py
@@ -1,41 +1,4 @@
-import re
 import os
-# from uuid import uuid4
-
-# DEFAULT_ENV = ".env"
-
-# PWD = os.environ.get("PWD") # Print Working Directory
-
-# def set_env(key: str, value: str=None, env_file: str=DEFAULT_ENV) -> bool:
-#     """
-#     env_file is recommended as 'ABSOLUTE PATH of the file'
-#     :return: true if successful
-#     """
-#     if value is None:
-#         value = ""
-#     os.environ[key] = value
-
-#     env_file = os.path.abspath(env_file)
-#     if not os.path.exists(env_file):
-#         raise FileNotFoundError
-   
-#     # Check if the variable already exists in the .env file
-#     with open(env_file, "r") as f:
-#         env_lines = f.readlines()
-#     variable_exists = False
-#     for i, line in enumerate(env_lines):
-#         if line.startswith(f"{key}="):
-#             env_lines[i] = f"{key}={value}\n"
-#             variable_exists = True
-
-#     # Add a new entry for the variable if it does not exist
-#     if not variable_exists:
-#         env_lines.append(f"{key}={value}\n")
-
-#     with open(env_file, "w") as f:
-#         f.writelines(env_lines)
-#     return True
-
 
 
 def set_env(key, value=None, env_file=".env"):
